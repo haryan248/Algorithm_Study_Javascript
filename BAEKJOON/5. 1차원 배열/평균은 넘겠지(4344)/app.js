bde00a040833336d8c3a51b35d77091193675e33
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-08-17 23:44:50 +0900

    백준 단계별 풀기 1차원 배열

--- BAEKJOON/5. 1차원 배열/평균은 넘겠지(4344)/app.js
+++ BAEKJOON/5. 1차원 배열/평균은 넘겠지(4344)/app.js
@@ -3,41 +3,14 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 let number = Number(input[0]);
-// for (let i = 1; i <= number; i++) {
-//     let cases = input[i].split(" ");
-//     let scoreCaseCount = Number(cases[0]);
-//     let sum = 0;
-//     arry = [...cases.splice(1)];
-//     sum = arry.reduce((sum, curr) => {
-//         return sum + Number(curr);
-//     }, 0);
-
-//     let reverage = sum / scoreCaseCount;
-
-//     let highScoreStudent = 0;
-//     console.log(cases);
-
-//     for (let k = 1; k <= scoreCaseCount; k++) {
-//         if (reverage < cases[k]) {
-//             highScoreStudent++;
-//         }
-//     }
-
-//     result = ((highScoreStudent / scoreCaseCount) * 100).toFixed(3);
-//     console.log(`${result}%`);
-// }
 for (let i = 1; i <= number; i++) {
     let cases = input[i].split(" ");
     let scoreCaseCount = Number(cases[0]);
     let sum = 0;
-
-    for (let j = 1; j <= scoreCaseCount; j++) {
-        sum += Number(cases[j]);
-    }
+    arry = [...cases];
+    sum = arry.splice(1).reduce((sum, curr) => sum + Number(curr), 0);
     let reverage = sum / scoreCaseCount;
-
     let highScoreStudent = 0;
-    console.log(cases);
     for (let k = 1; k <= scoreCaseCount; k++) {
         if (reverage < cases[k]) {
             highScoreStudent++;
@@ -45,5 +18,5 @@ for (let i = 1; i <= number; i++) {
     }
 
     result = ((highScoreStudent / scoreCaseCount) * 100).toFixed(3);
-    console.log(result + "%");
+    console.log(`${result}%`);
 }
