7f551bd7f944ade78ad31e74b24a59bdc825d47c
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-08-20 08:44:24 +0900

    그룹단어 체커 변경

--- BAEKJOON/7. 문자열/그룹 단어 체커(1316)/app.js
+++ BAEKJOON/7. 문자열/그룹 단어 체커(1316)/app.js
@@ -1,28 +1,27 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
-const caseCount = Number(input[0]);
-let countGroupWord = 0;
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+number = input[0];
+let cnt = 0;
+let testcaseArray;
+let before;
+for (let i = 1; i <= number; i++) {
+    testcaseArray = new Array(26).fill(0);
 
-for (let i = 1; i <= caseCount; i++) {
-    const word = input[i];
-    const letter = [];
-    let isGroupWord = true;
+    // input[i] = input[i].trim();
+    testcaseArray[input[i][0].charCodeAt(0) - 97] = 1;
 
-    for (let j = 0; j < word.length; j++) {
-        if (letter.indexOf(word[j]) === -1) {
-            letter.push(word[j]);
+    for (let j = 1; j < input[i].length; j++) {
+        if (input[i] !== input[i - 1] && testcaseArray[input[i][j].charCodeAt(0) - 97] !== 1) {
+            // 해당 알파벳index에 1넣기
+            testcaseArray[input[i][j].charCodeAt(0) - 97] = 1;
         } else {
-            if (letter.indexOf(word[j]) !== letter.length - 1) {
-                isGroupWord = false;
-                break;
-            }
+            console.log(input[i][j].charCodeAt(0));
+            console.log(input[i]);
+            break;
         }
-    }
-
-    if (isGroupWord) {
-        countGroupWord += 1;
+        if (j == input[i].length - 1) cnt++;
     }
 }
 
-console.log(countGroupWord);
+console.log(cnt);
