b90c5b2df8419fbb279cec36d49ee08c93b9d400
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-09-03 21:03:20 +0900

    백준 단계별 풀기 정수론 및 조합론

---
+++ BAEKJOON/17. 정수론 및 조합론/패션왕 신해빈(9375)/app.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+
+// 문제 풀이
+const n = +input[0];
+
+let cursor = 0;
+for (let i = 1; i <= n; i++) {
+    const map = {};
+    const t = +input[++cursor];
+    for (let j = 1; j <= t; j++) {
+        input[j + cursor] = input[j + cursor].split(" ");
+        const cloth = input[j + cursor][0];
+        const kinds = input[j + cursor][1];
+
+        if (map[kinds]) {
+            map[kinds].push(cloth);
+        } else {
+            map[kinds] = [cloth];
+        }
+    }
+    cursor += t;
+
+    let cnt = 1;
+    for (const key in map) {
+        cnt *= map[key].length + 1;
+    }
+    console.log(cnt - 1); // 아무것도 안입을 경우 제외
+}
