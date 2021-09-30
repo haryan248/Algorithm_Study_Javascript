e61a74bbdbbbab0522923294a5483a4352fdb007
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-09-30 21:37:34 +0900

    [update] 백준 단계별 풀기 - 동적계획법2

--- BAEKJOON/동적 계획법 2/파일 합치기(11066)/app.js
+++ BAEKJOON/동적 계획법 2/파일 합치기(11066)/app.js
@@ -13,27 +13,23 @@ for (let i = 0; i < T; i++) {
 
 //DP[i][j]는 i에서 j까지 합하는데 필요한 최소 비용이 된다.
 
-function solution(k, arr) {
-    let Sum = new Array(k + 1).fill(0);
+function solution(n, arr) {
+    let sum = new Array(n + 1).fill(0);
     arr.unshift(0);
-    for (let i = 1; i < k + 1; i++) {
-        Sum[i] = Sum[i - 1] + arr[i];
+    for (let i = 1; i < n + 1; i++) {
+        sum[i] = sum[i - 1] + arr[i];
     }
-    let dp = Array.from(Array(k + 1), () => Array(k + 1).fill(0));
+    let dp = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
 
-    console.log(Sum);
     let min = 0;
-
-    for (let i = 2; i < k + 1; i++) {
-        for (let j = 1; j < k + 2 - i; j++) {
-            for (let k = 0; k < i - 1; k++) {
-                min = Math.min(dp[j][j + k] + dp[j + k + 1][j + i - 1]);
-                // dp[j][j + i - 1] = min + (Sum[j + i - 1] - Sum[j - 1]);
+    for (let d = 1; d < n; d++) {
+        for (let x = 1; x + d <= n; x++) {
+            let y = x + d;
+            dp[x][y] = 987654321;
+            for (let t = x; t < y; t++) {
+                dp[x][y] = Math.min(dp[x][y], dp[x][t] + dp[t + 1][y] + sum[y] - sum[x - 1]);
             }
-            console.log(min);
         }
     }
-    // console.log(dp);
-    // console.log(dp[1][k]);
-    // console.log(k, arr);
+    console.log(dp[1][n]);
 }
