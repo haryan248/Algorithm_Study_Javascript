42d442a4c139d2b77108af5a10478e40ef7bb6cf
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-10-03 18:41:51 +0900

    [update] 백준 단계별 풀기 - 동적계획법2

--- BAEKJOON/동적 계획법 2/파일 합치기(11066)/app.js
+++ BAEKJOON/동적 계획법 2/파일 합치기(11066)/app.js
@@ -11,25 +11,35 @@ for (let i = 0; i < T; i++) {
     solution(k, arr);
 }
 
-//DP[i][j]는 i에서 j까지 합하는데 필요한 최소 비용이 된다.
+// dp[i][j]는 i에서 j까지 합하는데 필요한 최소 비용이 된다.
+// (10, 20, 30) => min[(1,2) + ((1,2), 3) + (1 + 2 + 3) or (2, 3) + (1 , (2, 3)) + (1, 2, 3)]
+// 점화식 : DP[i][j] = DP[i][k] + DP[k+1][j] + sum(A[i]~A[j])
+// (i ~ k)까지 비용의 합과 그 다음 (k+1지점부터 마지막 지점)까지의 합
 
 function solution(n, arr) {
-    let sum = new Array(n + 1).fill(0);
-    arr.unshift(0);
-    for (let i = 1; i < n + 1; i++) {
+    let sum = new Array(n).fill(0);
+    sum[0] = arr[0];
+    for (let i = 1; i < n; i++) {
         sum[i] = sum[i - 1] + arr[i];
     }
-    let dp = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+    sum.unshift(0);
+    let dp = Array.from(Array(n), () => Array(n).fill(0));
 
-    let min = 0;
-    for (let d = 1; d < n; d++) {
-        for (let x = 1; x + d <= n; x++) {
-            let y = x + d;
-            dp[x][y] = 987654321;
-            for (let t = x; t < y; t++) {
-                dp[x][y] = Math.min(dp[x][y], dp[x][t] + dp[t + 1][y] + sum[y] - sum[x - 1]);
+    for (let i = 1; i < n; i++) {
+        // n-1의 횟수만큼 반복
+        // i : 1 ~ n
+        console.log(`i : ${i}`);
+        for (let j = 0; j < n - i; j++) {
+            // 대각선 탐색
+            let y = j + i;
+            console.log(j, y);
+            dp[j][y] = Number.MAX_SAFE_INTEGER;
+            let tmp = sum[y + 1] - sum[j]; // (arr[j] ~ arr[y+1]) 까지의 합
+            // python : sum(pages[j: y + 1])
+            for (let k = j; k < y; k++) {
+                dp[j][y] = Math.min(dp[j][y], dp[j][k] + dp[k + 1][y] + tmp);
             }
         }
     }
-    console.log(dp[1][n]);
+    console.log(dp[0][n - 1]);
 }
