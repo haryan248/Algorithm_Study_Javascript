23e5c65d28bd80807ddf9da5d5f163a94b8bff74
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-10-04 23:43:15 +0900

    [add] 백준 단계별 풀기 - 동적 계획법2

--- BAEKJOON/동적 계획법 2/행렬 곱셈 순서(11049)/app.js
+++ BAEKJOON/동적 계획법 2/행렬 곱셈 순서(11049)/app.js
@@ -1,29 +1,40 @@
-function dp(start, end, arr, d) {
-    if (memo[start][end] !== undefined) {
-        return memo[start][end];
-    } else if (start === end - 1) {
-        memo[start][end] = 0;
-        return 0;
-    } else {
-        let min = Infinity;
-        for (let i = start + 1; i < end; i++) {
-            min = Math.min(min, dp(start, i, arr, d) + dp(i, end, arr, d) + d[start] * d[i] * d[end]);
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const N = Number(input.shift());
+let testcaseArray = [];
+for (let i = 0; i < N; i++) {
+    let temp = input[i].split(" ").map((i) => Number(i));
+    testcaseArray.push(temp);
+}
+let dp = new Array(N).fill(0).map(() => new Array(N).fill(0));
+// dp[i][j] i 에서 j 까지 곱하는 연산의 최소값
+// 점화식 DP[i][j] = min(DP[i][j], DP[i][k] + DP[k+1][j] + nums[i]*nums[k+1]*nums[j+1]) (i≤k<j)
+
+// (5 3) (3 2) (2 6) (6 4)
+// 1.
+
+for (let i = 1; i < N; i++) {
+    // n-1의 횟수만큼 반복
+    // i : 1 ~ n
+    // console.log(`i : ${i}`);
+    for (let j = 0; j < N - i; j++) {
+        // 대각선 탐색
+        let y = j + i;
+        // console.log(j, y);
+        dp[j][y] = Number.MAX_SAFE_INTEGER;
+
+        if (y - j === 1) {
+            dp[j][y] = testcaseArray[j][0] * testcaseArray[y][0] * testcaseArray[y][1];
+            continue;
         }
-        memo[start][end] = min;
-        return min;
+        // j부터 y 까지의 합,
+        // j ~ y 까지 나눠보면서 가장 작은값으로 갱신
+        // testcaseArray[j][0] * testcaseArray[k][1] * testcaseArray[y][1]) 는 두 부분배열의 곱셈
+        for (let k = j; k < y; k++) {
+            dp[j][y] = Math.min(dp[j][y], dp[j][k] + dp[k + 1][y] + testcaseArray[j][0] * testcaseArray[k][1] * testcaseArray[y][1]);
+        }
     }
 }
-
-const matrix = require("fs")
-    .readFileSync("/dev/stdin")
-    .toString()
-    .trim()
-    .split("\n")
-    .slice(1)
-    .map((el) => el.split(" ").map(Number));
-
-const memo = new Array(matrix.length + 1).fill(0).map((el) => new Array(matrix.length + 1));
-
-const d = new Array(matrix.length + 1).fill(0).map((el, idx) => (idx === 0 ? (el = matrix[0][0]) : (el = matrix[idx - 1][1])));
-
-console.log(dp(0, matrix.length, matrix, d));
+// console.log(dp);
+console.log(dp[0][N - 1]);
