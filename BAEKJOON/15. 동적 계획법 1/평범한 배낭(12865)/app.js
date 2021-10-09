be5ac3cfb8342b2696e82bd73bd96b1348bab0be
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-10-09 13:42:47 +0900

    [update] 동적계획법2 -> 23. 동적계획법2 파일면 변경

--- BAEKJOON/15. 동적 계획법 1/평범한 배낭(12865)/app.js
+++ BAEKJOON/15. 동적 계획법 1/평범한 배낭(12865)/app.js
@@ -14,17 +14,19 @@ for (let i = 1; i <= number; i++) {
 }
 console.log(testcaseArray);
 let dp = new Array(number + 1).fill(0).map(() => Array(k + 1).fill(0));
+// 가방에 담은 물건의 무게 합이 i일 때, 처음 j개의 물건 중 담을 수 있는 최대 가치 ( 1 < i <=M)
 for (let i = 1; i <= number; i++) {
     for (let j = 1; j <= k; j++) {
         let w = testcaseArray[i][0]; //해당 무게
         let v = testcaseArray[i][1]; //해당 가치
 
         if (j < w) {
-            //들 수 없으므로 이전의 최대값
+            //들 수 없으므로 넣기 이전의 최대값
             dp[i][j] = dp[i - 1][j];
         } else {
             dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
-            // 이전의 최대 가치, w를 들었을 때의 가치중 max 값
+            // 이전의 최대 가치와 w를 들었을 때의 가치 중 max 값
+            // w를 들었을 때의 최대 값은 w를 뺸만큼 + 현재 무게의 값
         }
     }
 }
