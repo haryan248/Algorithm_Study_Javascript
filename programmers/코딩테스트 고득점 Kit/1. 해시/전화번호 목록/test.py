55250c0dfd17d36e4ad4fbf4e23c3819491d644a
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-10-28 12:35:33 +0900

    [add] 프로그래머스 고득점 kit - JS 대신 python

---
+++ programmers/코딩테스트 고득점 Kit/1. 해시/전화번호 목록/test.py
@@ -0,0 +1,15 @@
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            # 각 자리 수
+            # print(number)
+            temp += number
+            print(temp)
+            if temp in hash_map and temp != phone_number:
+                return False
+    return answer
