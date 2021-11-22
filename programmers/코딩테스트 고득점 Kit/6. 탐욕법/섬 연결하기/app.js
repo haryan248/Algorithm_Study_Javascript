79a497ff67ec8f3ada82813fc87f4fd60ef0a56f
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-11-22 18:57:56 +0900

    [update] 코딩테스트 고득점 kit 탐욕법

--- programmers/코딩테스트 고득점 Kit/6. 탐욕법/섬 연결하기/app.js
+++ programmers/코딩테스트 고득점 Kit/6. 탐욕법/섬 연결하기/app.js
@@ -1,35 +1,36 @@
-function solution(N, costs) {
-    const INF = Number.MAX_VALUE;
-    var answer = 0;
-    let graph = new Array(N + 1).fill(0).map(() => new Array(N + 1).fill(INF));
+const getParent = (parent, x) => {
+    if (parent[x] === x) return x;
+    return (parent[x] = getParent(parent, parent[x]));
+};
 
-    // 대각선의 비용은 0으로 초기화
-    // 자기자신으로 가는 비용은 없으므로
-    for (let i = 1; i < N + 1; i++) {
-        for (let j = 1; j < N + 1; j++) {
-            if (i === j) {
-                graph[i][j] = 0;
-            }
-        }
-    }
+const unionParent = (parent, a, b) => {
+    const n1 = getParent(parent, a);
+    const n2 = getParent(parent, b);
+    if (n1 < n2) return (parent[n2] = n1);
+    else return (parent[n1] = n2);
+};
+
+const findParent = (parent, a, b) => {
+    const n1 = getParent(parent, a);
+    const n2 = getParent(parent, b);
+    if (n1 === n2) return true;
+    else return false;
+};
+
+function solution(n, costs) {
+    let answer = 0;
+    const parent = [];
+    for (let i = 0; i < n; i++) parent.push(i);
+
+    costs.sort((a, b) => a[2] - b[2]);
 
     for (const cost of costs) {
-        let [a, b, c] = cost;
-        graph[a][b] = Math.min(graph[a][b], c);
-    }
-    console.log(graph);
-
-    for (let k = 1; k < N + 1; k++) {
-        for (let i = 1; i < N + 1; i++) {
-            for (let j = 1; j < N + 1; j++) {
-                graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
-            }
+        if (!findParent(parent, cost[0], cost[1])) {
+            answer += cost[2];
+            unionParent(parent, cost[0], cost[1]);
         }
     }
-    for (let i = 1; i < N; i++) {
-        answer += graph[0][i];
-    }
-    // console.log(graph);
+
     return answer;
 }
 
